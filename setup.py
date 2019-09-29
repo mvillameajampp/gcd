bc77a43130c85ebb4a42943f673902316d3d3943
memeplex <carlosjosepita@gmail.com>
2019-09-29 20:50:25 -0300

    Remove documentation related scripts and resources

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup
 
 
-setup(
-    name="gcd",
-    version="1.0",
-    packages=["gcd"],
-    package_data={"gcd": ["resources/*"]},
-    scripts=["scripts/docdoc", "scripts/wacky"],
-)
+setup(name="gcd", version="1.0", packages=["gcd"])
