d6bb1ca04f8b725834a5dcb59f272d72fabe1b30
tzulberti-jampp <tomas@jampp.com>
2019-10-22 19:04:40 -0300

    UB-119: Install pyscopg2 when running the tests (#40)
    * UB-119: Install pyscopg2 when running the tests
    
    * UB-119: Reformat setup.py using black

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+import os
 from setuptools import setup
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-setup(name="gcd", version="1.0", packages=["gcd"])
+
+def parse_requirements_txt(filename="requirements.txt"):
+    with open(os.path.join(current_dir, filename)) as requirements_file:
+        requirements = requirements_file.readlines()
+        # remove whitespaces
+        requirements = [line.strip().replace(" ", "") for line in requirements]
+        # remove all the requirements that are comments
+        requirements = [line for line in requirements if not line.startswith("#")]
+        # remove empty lines
+        requirements = list(filter(None, requirements))
+        return requirements
+
+
+setup(
+    name="gcd",
+    version="1.0",
+    packages=["gcd"],
+    extras_require={
+        "dev": parse_requirements_txt("requirements-dev.txt"),
+        "all": parse_requirements_txt("requirements.txt"),
+    },
+)
